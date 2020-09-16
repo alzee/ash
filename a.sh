610af7c8253e92756d70a51bf7ccb141d99089ae
Al Zee <z@alz.ee>
2020-09-16 16:34:05 +0800

    now we know why mpm_prefork not disabled

--- a.sh
+++ a.sh
@@ -342,14 +342,9 @@ misc() {
         # mod_http2 doesn't work with mpm_prefork'
         # and "event mpm is nowadays the best one"
         # https://httpd.apache.org/docs/2.4/howto/http2.html#mpm-config
-        sudo a2dismod mpm_prefork $php
+        sudo a2dismod $php mpm_prefork # if other version of mod_php is enabled, mpm_prefork won't be disabled due to dependency
         sudo a2enmod mpm_event http2 rewrite ssl socache_shmcb headers proxy_fcgi setenvif
         sudo a2enconf $php-fpm
-		# sudo ln -s ../mods-available/rewrite.load /etc/apache2/mods-enabled/
-		# sudo ln -s ../mods-available/ssl.load /etc/apache2/mods-enabled/
-		# sudo ln -s ../mods-available/ssl.conf /etc/apache2/mods-enabled/
-		# sudo ln -s ../mods-available/socache_shmcb.load /etc/apache2/mods-enabled/
-		# sudo ln -s ../mods-available/headers.load /etc/apache2/mods-enabled/
 
 		sudo ln -s ~/vhosts /etc/apache2/sites-enabled/
 		sudo ln -s ~/vhosts.conf /etc/apache2/conf-enabled/
