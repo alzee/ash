d63d0f27d1b0c5dceb9e95f59d4acff5562915b0
Al Zee <z@alz.ee>
2021-05-12 14:16:31 +0800

    mailutils

--- a.sh
+++ a.sh
@@ -110,7 +110,7 @@ _init() {
 			php_ver=$(apt list php -a | grep testing | cut -d':' -f2)
 			php=php${php_ver%+*}
 			php_with_exts=$(echo $php-{common,cli,xml,gd,opcache,apcu,mbstring,zip,mysql,curl,json,fpm,dev,uploadprogress})
-			ilist="$ilist apache2 $php_with_exts pkg-php-tools mariadb-server firewalld redis-server python3-pip psmisc xz-utils bzip2 man-db"
+			ilist="$ilist apache2 $php_with_exts pkg-php-tools mariadb-server firewalld redis-server python3-pip psmisc xz-utils bzip2 man-db mailutils"
 			# unixodbc unixodbc-dev selinux-basics selinux-policy-default auditd"
 			# libapache2-mod-$php apache2-dev libssl-dev libxml2-dev libcurl3-dev libpng-dev pkg-config lsb-release
 			# Run selinux-activate(as root) to configure GRUB and PAM and to create /.autorelabel
