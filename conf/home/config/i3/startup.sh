52b98f7f1be3aed4d48078b7b30146a587296d23
Al Zee <z@alz.ee>
2022-02-24 22:57:13 +0800

    nth

--- conf/home/config/i3/startup.sh
+++ conf/home/config/i3/startup.sh
@@ -10,13 +10,13 @@
 
 ibus-daemon -rxd
 
-[ -f ~/.config/i3/startup.local.sh ] && . ~/.config/i3/startup.local.sh
-
 feh --bg-fill $bg
-
-#i3-msg workspace 1
-i3-sensible-terminal
 
 touchpad=$(xinput --list --name-only | grep -i touchpad)
 [ "$touchpad" ] &&
     xinput --set-prop "$touchpad" 'libinput Tapping Enabled' 1
+
+[ -f ~/.config/i3/startup.local.sh ] && . ~/.config/i3/startup.local.sh
+
+#i3-msg workspace 1
+i3-sensible-terminal
