621d0a57710fc63f371b4b23b7e761c3fb1d57f0
Al Zee <z@alz.ee>
2022-03-17 20:56:02 +0800

    nth

--- geta.sh
+++ geta.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-# url=https://github.com/alzee/ash/archive/refs/heads/master.zip
-url=https://gitlab.com/alzee/ash/-/archive/master/ash-master.tar.gz
-user=al
-f=${url##*/}
-
 # add user
+user=al
 sudo useradd -m -s /bin/bash $user
 sudo usermod -aG sudo $user
 sudo cp .ssh/  /home/$user/ -a
@@ -13,6 +9,9 @@ sudo chown -R $user:$user /home/$user/.ssh
 echo $user:zee | sudo chpasswd
 
 # get a.sh
+#url=https://github.com/alzee/ash/archive/refs/heads/master.zip
+url=https://gitlab.com/alzee/ash/-/archive/master/ash-master.tar.gz
+f=${url##*/}
 curl -O "$url"
 tar xf $f
 rm $f
