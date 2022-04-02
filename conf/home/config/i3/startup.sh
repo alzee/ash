d7acf8c3391734bff4a57060fd280b0c80b7ab95
Al Zee <z@alz.ee>
2022-04-02 23:51:47 +0800

    no need to handle touchpad/mouse in i3 startup since we have xorg conf now

--- conf/home/config/i3/startup.sh
+++ conf/home/config/i3/startup.sh
@@ -12,13 +12,6 @@ ibus-daemon -rxd
 
 feh --bg-fill $bg
 
-touchpad=$(xinput --list --name-only | grep -i touchpad)
-[ "$touchpad" ] &&
-    xinput --set-prop "$touchpad" 'libinput Tapping Enabled' 1
-
-# Why xinput not working if put it after i3-sensible-terminal?
-lhand
-
 [ -f ~/.config/i3/startup.local.sh ] && . ~/.config/i3/startup.local.sh
 
 #i3-msg workspace 1
