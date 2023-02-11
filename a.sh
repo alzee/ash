d61e250b1ff7a9e54d45cb3c69cd6a260d45c876
Al Zee <z@alz.ee>
2023-02-11 21:20:46 +0800

    gh repo for debian

--- a.sh
+++ a.sh
@@ -95,7 +95,11 @@ add_repo() {
             # add testing repo (latest packages)
             sudo cp $scriptdir/conf/templates/debian/testing.list /etc/apt/sources.list.d/
             # curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
-            
+            # github cli
+            curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+                && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+                && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+            # docker
             curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
             echo \
                 "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] \
@@ -485,17 +489,6 @@ install_rust(){
     rustup completions bash cargo >> ~/.local/share/bash-completion/completions/cargo
 }
 
-install_gh(){
-    if [ "$distro" = debian ]; then
-        type -p curl >/dev/null || sudo apt install curl -y
-        curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
-            && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
-            && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-            && sudo apt update \
-            && sudo apt install gh -y
-    fi
-}
-
 enable_networkmanager(){
     # Since we remove dhcpcd5
     # sudo echo denyinterfaces wlan0 >> /etc/dhcpcd.conf
@@ -550,9 +543,6 @@ case $1 in
         ;;
     -R)
         install_rust
-        ;;
-    -G)
-        install_gh
         ;;
     -D)
         default_pool
