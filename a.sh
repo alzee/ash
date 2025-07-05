c219121005ef43b4b00bc0a400bddb439bb0eaa7
Al Zee <z@alz.ee>
2025-07-05 15:55:03 +0800

    no more gh cli , mariadb for debian

--- a.sh
+++ a.sh
@@ -94,10 +94,10 @@ add_repo() {
             sudo cp $scriptdir/conf/templates/debian/testing.list /etc/apt/sources.list.d/
             # curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
             # github cli
-            curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
-                && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
-                && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
-            # docker
+            # curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+            #     && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+            #     && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+            # # docker
             curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
             echo \
                 "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] \
